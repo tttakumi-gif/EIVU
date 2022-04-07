ec9fb79cb0d6a992ef16a2063fb0a21717b08bed
taked1664 <taked1664@gmail.com>
2022-04-07 18:07:07 +0900

    add struct

--- buffer.hpp
+++ buffer.hpp
@@ -32,12 +32,39 @@ struct desc {
 	int16_t flags;
 };
 
+struct vring_desc {
+	uint64_t addr;
+	uint32_t len;
+	uint16_t flags;
+	uint16_t next;
+};
+
+struct vring_avail {
+	uint16_t flags;
+	uint16_t idx;
+	uint16_t ring[0];
+};
+
+struct vring_used_elem {
+	uint32_t id;
+	uint32_t len;
+};
+
+struct vring_used {
+	uint16_t flags;
+	uint16_t idx;
+	struct vring_used_elem ring[0];
+};
+
 struct ring {
 	int16_t size;
 	int16_t last_avail_idx;
 	int16_t last_used_idx;
 	int32_t pool_index;
 	desc descs[SIZE_RING];
+	vring_desc vdesc[SIZE_RING];
+	vring_avail vavail[SIZE_RING];
+	vring_used vused[SIZE_RING];
 }__attribute__((__aligned__(64)));
 
 #include "cbuffer.hpp"
