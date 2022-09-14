90a4f53b0afc954653ccd62091bdf0d18fa603fc
taked1664 <taked1664@gmail.com>
2022-09-14 22:31:43 +0900

    update .gitignore

--- cmake-build-debug/singlerun.sh
+++ cmake-build-debug/singlerun.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #opt_perf="-e l1d.replacement,l1d_pend_miss.fb_full,l1d_pend_miss.pending,l2_lines_in.all,L1-dcache-loads,L1-dcache-load-misses,L1-dcache-stores,L1-dcache-store-misses,LLC-loads,LLC-load-misses,LLC-stores,LLC-store-misses,l2_rqsts.all_rfo,l2_rqsts.rfo_hit,l2_rqsts.rfo_miss,offcore_response.demand_rfo.l3_hit.hit_other_core_no_fwd,offcore_requests_outstanding.cycles_with_demand_rfo"
-opt_perf="-e cache-references,cache-misses,mem_load_uops_retired.l1_hit,mem_load_uops_retired.l1_miss,mem_load_uops_retired.l3_hit,mem_load_uops_retired.l3_miss,LLC-load,LLC-load-misses,LLC-store,LLC-store-misses"
+opt_perf="-e cache-references,cache-misses,mem_load_uops_retired.l1_hit,mem_load_uops_retired.l1_miss,mem_load_uops_retired.hit_lfb,mem_load_uops_retired.l3_hit,mem_load_uops_retired.l3_miss,LLC-load,LLC-load-misses,LLC-store,LLC-store-misses"
 #l2_rqsts.all_rfo,l2_rqsts.rfo_hit,l2_rqsts.rfo_miss,offcore_requests.demand_rfo,offcore_requests_outstanding.demand_rfo,mem_inst_retired.all_loads,mem_inst_retired.all_stores
 
 #opt_perf="-e cache-misses,L1-dcache-load-misses,L1-dcache-loads,L1-dcache-stores,LLC-loads,LLC-stores,LLC-load-misses,LLC-store-misses,cpu/event=0xA3,umask=0x06,cmask=0x06,name=STALLS_L3_MISS/"
